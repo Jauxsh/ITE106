2002348b9557b2322eaeeafa349ad92c3235a82d
Jau <111845557+Jauxsh@users.noreply.github.com>
2024-10-13 19:36:35 +0800

    Not Final

--- 02TextFileManipulationScannerLoop/Main.java
+++ 02TextFileManipulationScannerLoop/Main.java
@@ -1,1 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File myFile = new File("JauxshFile.txt");
+        Scanner fileScanner = new Scanner(myFile);
+        
+        while (fileScanner.hasNextLine()) {
+            System.out.println(fileScanner.nextLine());
+        }
+        
+        fileScanner.close();
+    }
+}
