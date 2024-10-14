a4f995739b9f70d518441bde077d39383bf25e22
Jau <111845557+Jauxsh@users.noreply.github.com>
2024-10-14 17:01:21 +0800

    Update Main.java

--- 05TextFileManipulationFileReader/Main.java
+++ 05TextFileManipulationFileReader/Main.java
@@ -1,1 +1,16 @@
+import java.io.File;
+import java.io.FileReader;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File myFile = new File("JauxshFile.txt");
+        FileReader fr = new FileReader(myFile);
+        
+        int i;
+        while ((i = fr.read()) != -1) {
+            System.out.print((char) i);
+        }
+
+        fr.close();
+    }
+}
