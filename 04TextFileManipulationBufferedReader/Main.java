978d503dbad5cecd68257fd3c37f26a00eb67df3
Jau <111845557+Jauxsh@users.noreply.github.com>
2024-10-14 15:02:01 +0800

    Update Main.java

--- 04TextFileManipulationBufferedReader/Main.java
+++ 04TextFileManipulationBufferedReader/Main.java
@@ -1,1 +1,17 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File file = new File("JauxshFile.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+    }
+}
