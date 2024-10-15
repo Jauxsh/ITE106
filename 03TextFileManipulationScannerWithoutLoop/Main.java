e6d201aa3458d5ba5936450da0d68cebeac56ef7
Jau <111845557+Jauxsh@users.noreply.github.com>
2024-10-15 14:55:48 +0800

    Update Main.java

--- 03TextFileManipulationScannerWithoutLoop/Main.java
+++ 03TextFileManipulationScannerWithoutLoop/Main.java
@@ -2,11 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
  
-public class Maih {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("JauxshFile.txt");
         Scanner sc = new Scanner(file);
+     
         sc.useDelimiter("\\Z");
         System.out.println(sc.next());
+     
+        sc.close();
     }
 }
