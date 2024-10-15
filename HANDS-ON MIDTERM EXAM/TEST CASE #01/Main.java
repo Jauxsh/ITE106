d56d1622a2d9096ce6ef1a6cbbf7520213ad538b
Jau <111845557+Jauxsh@users.noreply.github.com>
2024-10-15 16:18:41 +0800

    adjusted variable names

--- HANDS-ON MIDTERM EXAM/TEST CASE #01/Main.java
+++ HANDS-ON MIDTERM EXAM/TEST CASE #01/Main.java
@@ -2,45 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the number of students: ");
-        int StudentNum = sc.nextInt();
-        sc.nextLine();
+        int StudentNum = scanner.nextInt();
+        scanner.nextLine();
 
         for (int i = 0; i < StudentNum; i++) {
             System.out.print("Enter the name of student " + (i + 1) + ": ");
-            String StudentName = sc.nextLine();
+            String StudentName = scanner.nextLine();
 
             System.out.print("Enter the number of assignments for " + StudentName + ": ");
-            int AssignmentNum = sc.nextInt();
+            int AssignmentNum = scanner.nextInt();
             double totalScore = 0;
 
             for (int j = 0; j < AssignmentNum; j++) {
                 System.out.print("Enter score for assignment " + (j + 1) + ": ");
-                double score = sc.nextDouble();
+                double score = scanner.nextDouble();
                 totalScore += score;
             }
 
-            double average = totalScore / AssignmentNum;
-            char grade;
+            double AverageGrade = totalScore / AssignmentNum;
+            char grademark;
 
-            if (average >= 90) {
-                grade = 'A';
-            } else if (average >= 80) {
-                grade = 'B';
-            } else if (average >= 70) {
-                grade = 'C';
-            } else if (average >= 60) {
-                grade = 'D';
+            if (AverageGrade >= 90) {
+                grademark = 'A';
+            } else if (AverageGrade >= 80) {
+                grademark = 'B';
+            } else if (AverageGrade >= 70) {
+                grademark = 'C';
+            } else if (AverageGrade >= 60) {
+                grademark = 'D';
             } else {
-                grade = 'F';
+                grademark = 'F';
             }
 
-            sc.nextLine();
-            System.out.println("\n" + StudentName + " - Average: " + average + " - Grade: " + grade);
+            scanner.nextLine();
+            System.out.println("\n" + StudentName + " - Average: " + AverageGrade + " - Grade: " + grademark);
         }
 
-        sc.close();
+        scanner.close();
     }
 }
